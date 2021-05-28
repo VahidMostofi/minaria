05c852008f44ca8607f20428d98f7fc2570da871
Vahid <vahidmostofi@outlook.com>
2021-05-27 21:10:08 -0600

    remove health check test

--- main_test.go
+++ main_test.go
@@ -1,21 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"testing"
+// func TestHealthCheck(t *testing.T) {
+// 	cfg := client.DefaultTransportConfig().WithHost("localhost:9090")
+// 	c := client.NewHTTPClientWithConfig(nil, cfg)
+// 	r, err := c.Heath.CheckHealthStatus(heath.NewCheckHealthStatusParams())
 
-	"github.com/vahidmostofi/minaria/sdk/client"
-	"github.com/vahidmostofi/minaria/sdk/client/heath"
-)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-func TestHealthCheck(t *testing.T) {
-	cfg := client.DefaultTransportConfig().WithHost("localhost:9090")
-	c := client.NewHTTPClientWithConfig(nil, cfg)
-	r, err := c.Heath.CheckHealthStatus(heath.NewCheckHealthStatusParams())
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	fmt.Println(r)
-}
+// 	fmt.Println(r)
+// }
